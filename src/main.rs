4deb80c2f819cf4d710f1bc8d424e354e82c844c
Hayden Flinner <haydenflinner@gmail.com>
2022-09-09 07:47:13 -0500

    fourth commit?

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,10 @@ use chrono::prelude::*;
 use std::collections::BTreeMap;
 use std::error::Error;
 
-use ansi_term::Colour::Red;
+use ansi_term::Colour::Purple;
 use memmap::Mmap;
 use memmap::MmapOptions;
+use std::cmp::min;
 use std::fs::File;
 use unicode_width::UnicodeWidthStr; // To get the width of some text.
 
@@ -60,14 +61,9 @@ fn find_line_starting_before(mmap: &Mmap, byte_offset: usize) -> usize {
     return byte_offset - bytes_before_offset_of_newline;
 }
 
-fn init_page_for_offset(page_table: &mut PageTable, mmap: &Mmap, offset: usize) {
-    // let offset = offset - (offset % PG_SIZE);
-    let pgno = PageNo::new(offset);
-    if page_table.contains_key(&pgno) {
-        return;
-    }
-    let start_offset = find_line_starting_before(mmap, pgno.as_byte_offset());
-    let s = std::str::from_utf8(&mmap[start_offset..start_offset + 100]).unwrap();
+fn parse_date_starting_at(s: &[u8], start_offset: usize) -> DateTime<Utc> {
+    // TODO .. need to use min or is that implicit like Python?
+    let s = std::str::from_utf8(&s[start_offset..min(start_offset + 100, s.len())]).unwrap();
     let second_space_idx = s
         .char_indices()
         .filter_map(|(index, char)| match char == ' ' {
@@ -80,10 +76,147 @@ fn init_page_for_offset(page_table: &mut PageTable, mmap: &Mmap, offset: usize) 
     eprintln!("Parsing: {}", s);
     // match dateparser::parse(std::str::from_utf8(&s).unwrap()) {
     match dateparser::parse(s) {
-        Ok(dt) => page_table.insert(pgno, Page { start_time: dt }),
+        Ok(dt) => dt,
         _ => panic!(),
-    };
+    }
 }
+
+fn init_page_for_offset(page_table: &mut PageTable, mmap: &Mmap, offset: usize) {
+    // let offset = offset - (offset % PG_SIZE);
+    let pgno = PageNo::new(offset);
+    if page_table.contains_key(&pgno) {
+        return;
+    }
+    let start_offset = find_line_starting_before(mmap, pgno.as_byte_offset());
+    page_table.insert(
+        pgno,
+        Page {
+            start_time: parse_date_starting_at(mmap, start_offset),
+        },
+    );
+}
+
+/*
+fn get_view(mmap: &Mmap, offset: usize) {
+    //
+
+}
+*/
+
+struct App<'a> {
+    mmap: &'a Mmap,
+    // current byte offset into mmapped file.
+    cursor: usize,
+}
+
+type FileOffset = usize;
+
+impl App<'_> {
+    fn get_view(&self) -> &str {
+        std::str::from_utf8(&self.mmap[self.cursor..PG_SIZE * 4]).unwrap() // 4kb, TODO Could minimize this by knowing size of terminal+rows returned.
+    }
+
+    fn next_line(&mut self) {
+        // let bytes_til_newline = &mmap[cursor..cursor.PG_SIZE*4]
+        let bytes_til_newline = self
+            .mmap
+            .iter()
+            .enumerate()
+            .find_map(|(index, val)| match val {
+                val if *val == ('\n' as u8) => Some(index),
+                _ => None,
+            })
+            .unwrap_or(0);
+        let byte_after_newline = min(self.mmap.len(), self.cursor + bytes_til_newline + 1);
+        self.cursor = byte_after_newline;
+    }
+
+    fn prev_line(&mut self) {
+        self.cursor = find_line_starting_before(self.mmap, self.cursor);
+    }
+
+    fn goto_begin(&mut self) {
+        self.cursor = 0;
+    }
+
+    fn goto_end(&mut self) {
+        // TODO Just set cursor to mmap.len()? Does cursor really need to be at beginning of valid line always?
+        self.cursor = find_line_starting_before(self.mmap, self.mmap.len());
+    }
+}
+
+struct NavHandler<'a> {
+    mmap: &'a Mmap,
+    pt: PageTable,
+}
+
+impl NavHandler<'_> {
+    fn new(mmap: &Mmap) -> NavHandler {
+        let nh = NavHandler {
+            mmap,
+            pt: PageTable::new(),
+        };
+        load_initial_pages(&mut nh.pt, mmap);
+        nh
+    }
+
+    fn goto(self, spec: &str) {
+        todo!();
+    }
+
+    fn goto_time(self, spec: &DateTime<Utc>) {
+        // Walk the page table, populating entries as needed to find the right page.
+        // Better abstractino would probably be a pagetable holder that populates itself.
+        // Perhaps a vector of PageInfos, and just use stdlib binary search?
+        // This is dumb, and a better approach would be to do binary search, and where we see a gap between two pages,
+        // (a page that we haven't checked the timestamp of), go and fetch that page and find ts, effectively a lazy search through the text.
+        // Unfortunately we can't take mutable reference to self in cmp/partialcmp, fair enough.
+        // What we can do is, given the start and end times of each page, look for the given ts.
+        // If we don't find it, then we can add a few missing pages into the page table to look for it.
+        // If
+
+        // Assume we know nothing. What we must do is binary search through the text file, that's not that bad.
+        // Then, to save this work from being done all of the time (even though it's more or less intant, it's not quite interactive)
+        // Cache the start/end times of each page that we loaded in the process of finding this timestamp, for future finding.
+
+        // TODO Check pt cache here. Store the spot that we should insert page table entries that we find.
+        // Or maybe even a starting point, although frankly it's pointless.
+        let spot = bin_search_file(self.mmap, spec, s.len() / 2);
+    }
+}
+
+enum TsBinSearchError {
+    FailedParseTs,
+}
+
+// fn bin_search_file(s: &[u8], dt: &DateTime<Utc>) {
+fn bin_search_file_helper(
+    s: &[u8],
+    dt: &DateTime<Utc>,
+    byte_offset: usize,
+) -> Result<FileOffset, TsBinSearchError> {
+    let line_start = find_line_starting_before(&s, byte_offset);
+    let found_dt = parse_date_starting_at(s, line_start);
+    if line_start == 0 {
+        return Ok(byte_offset);
+    }
+    // TODO assumption here that every newline starts with a timestamp.
+    // Should be resilient to multiline-logs, maybe search back a whole few mb or so looking for the prev timestamp.
+    // Algo: go left/right until we can't anymore, by comparing the byte idx of ideal move point vs prev newline.
+
+    match cmp.compare(dt, found_dt) {
+        Less => x,    // Found is after, go left
+        Equal => y,   // Wow, what a guess lol
+        Greater => z, // Found is before, go right.
+    }
+}
+
+/*/
+#[derive(Default)]
+struct PageTimeInfo {
+    start_time: Optional<DateTime<Utc>>,
+}
+*/
 
 fn load_initial_pages(page_table: &mut PageTable, mmap: &Mmap) {
     init_page_for_offset(page_table, mmap, 0);
@@ -118,25 +251,31 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut siv = cursive::default();
 
-    // We can quit by pressing q
+    let viewsize = PG_SIZE * 4; // 16kb
+                                // We can quit by pressing q
     siv.add_global_callback('q', |s| s.quit());
 
-    /*/
     siv.add_fullscreen_layer(
-        TextView::new("hello")
-            .wrap_with(OnEventView::new)
-            .on_pre_event_inner(Event::Char('x'), move |v, _| {
-                v.set_content(Red.paint(std::str::from_utf8(&mmap).unwrap()).to_string());
-                Some(EventResult::Consumed(None))
-            })
-            .on_pre_event_inner(Event::Char('y'), move |v, _| {
-                v.set_content("hello");
-                Some(EventResult::Consumed(None))
-            }),
+        TextView::new(cursive::utils::markup::ansi::parse(
+            Purple.paint("hello").to_string(),
+        ))
+        .wrap_with(OnEventView::new)
+        .on_pre_event_inner(Event::Char('x'), move |v, _| {
+            v.set_content(cursive::utils::markup::ansi::parse(
+                Purple
+                    .paint(std::str::from_utf8(&mmap[byte_offset..byte_offset + viewsize]).unwrap())
+                    .to_string(),
+            ));
+            Some(EventResult::Consumed(None))
+        })
+        .on_pre_event_inner(Event::Char('y'), move |v, _| {
+            v.set_content("hello");
+            Some(EventResult::Consumed(None))
+        }),
     );
-    */
     // let state = String::from("hello");
     /* */
+    /*
     let state: String = Red.paint("hello").to_string();
     siv.add_fullscreen_layer(
         Canvas::new(state)
@@ -146,6 +285,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             })
             .with_required_size(|text, _constraints| (text.width(), 1).into()),
     );
+    */
 
     siv.run();
     Ok(())
