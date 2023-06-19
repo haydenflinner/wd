785c289b516684dbcd7e551834f1b934796ba1bf
Hayden Flinner <haydenflinner@gmail.com>
2023-06-19 17:00:05 -0400

    added microbenches

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use bstr::ByteSlice;
 
+use chrono::Local;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use memmap::MmapOptions;
 use wd::components::home::{get_visible_lines};
@@ -14,6 +15,11 @@ fn criterion_benchmark(c: &mut Criterion) {
     // Not the bottleneck, go for it.
     c.bench_function("hugefile.txt getviz", |b| b.iter(|| get_visible_lines(black_box(mmap.as_bstr()), &vec!(), 10000, 10000, 0)));
     // c.bench_function("hugefile.txt getviz copy", |b| b.iter(|| get_visible_lines_slow(black_box(mmap.as_bstr()), &vec!(), 10000, 10000)));
+    let s = "04/04/1997 12:04:01";
+    // 40GB * (1.7microseconds / 311kb) ~= 200ms for a 40GB file ignoring cache misses, good enough.
+    c.bench_function("dateparser generic", |b| b.iter(|| dateparser::parse_with_timezone(black_box(s), &Local) ) );
+    // 67ns to parse the epoch timestamp though, dang, must be nice.
+    c.bench_function("dateparser generic epoch", |b| b.iter(|| dateparser::parse_with_timezone(black_box("1687208330"), &Local) ) );
 }
 
 criterion_group!(benches, criterion_benchmark);
